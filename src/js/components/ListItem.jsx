df1625988661917a519f774e8edab883cacef9b0
JosueJMartinez <jmarti61@gmail.com>
2020-09-09 12:58:24 -0500

    updated adding edit feature still broken in progress

--- src/js/components/ListItem.jsx
+++ src/js/components/ListItem.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 
 export default class ListItem extends Component {
+	state = {
+		content: this.props.content,
+		isEdit: false
+	};
 	handleDelete = e => {
 		this.props.deleteItem(this.props.id);
 	};
 
+	handleEdit = e => {
+		this.setState({ isEdit: true });
+	};
+
+	handleChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
 	render() {
 		return (
 			<div className="ListItem" id={this.props.id}>
-				{this.props.content}
-				<button>edit</button>
+				{this.state.isEdit ? (
+					<input
+						type="text"
+						placeholder="Editing item"
+						value={this.state.content}
+						name="content"
+						onChange={this.handleChange}
+					/>
+				) : (
+					this.props.content
+				)}
+				<button onClick={this.handleEdit}>edit</button>
 				<button onClick={this.handleDelete}>delete</button>
 			</div>
 		);
